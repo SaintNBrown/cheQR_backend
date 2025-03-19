56b976a4ad2ab4a2fa7a4faa38a9c0c0fa505f58
Nathaniel David Brown <brownbrown@gmail.com>
2025-03-19 12:00:05 +0100

    cleared node_modules

--- cheQR/src/main/java/qubiqule/cheqr/cheQR/CheQrApplication.java
+++ cheQR/src/main/java/qubiqule/cheqr/cheQR/CheQrApplication.java
@@ -2,13 +2,21 @@ package qubiqule.cheqr.cheQR;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableScheduling
+@PropertySource("classpath:application.properties")
 public class CheQrApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(CheQrApplication.class, args);
 	}
 
